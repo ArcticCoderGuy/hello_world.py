efc850f773b9b0a1073407f32d52e9f1671cc7a2
Markus Kaprio <markus.kaprio@edunord.se>
2025-11-05 15:46:40 +0200

    large append

--- Motorcycles.P36-41.py
+++ Motorcycles.P36-41.py
@@ -1,6 +1,10 @@
-motorcycles =["Honda", "Yamaha", "Suzuki"]
-print(motorcycles)
-      
-motorcycles.append("Ducati")
-print(motorcycles)
+motorcycles = []
 
+motorcycles.append('Honda')
+motorcycles.append('Yamaha')
+motorcycles.append('Suzuki') 
+motorcycles.append('Kawasaki')  
+motorcycles.append('Ducati')    
+
+
+print(motorcycles)
