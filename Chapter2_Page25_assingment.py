eb26b67a8afe518660a779aeb01e8faa37cb25d4
Markus Kaprio <markus.kaprio@edunord.se>
2025-12-08 16:21:20 +0200

    final version of Hello World

--- Chapter2_Page25_assingment.py
+++ Chapter2_Page25_assingment.py
@@ -1,9 +1,22 @@
-def message_1():
-    """ Harjoitus 2-1"""
-    message = "Hello Python World"
+message = "Hello, World!"
+print(message)
 
-    return message
+
+
+def message_1():
+    """ chapter 1 assignment v2.0"""
+    message = "Hello, World!"
+    return message  
+
 print(message_1())
 
 
-     
+def message_2():
+    """ chapter 1 assignment v3.0"""
+    message = "Hello, Python World!"
+    return message.title()
+
+print(message_2())
+
+
+    
