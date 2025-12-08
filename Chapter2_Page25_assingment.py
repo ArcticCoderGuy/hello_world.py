850f42331f94d2d9d03b56320522407b46214605
Markus Kaprio <markus.kaprio@edunord.se>
2025-12-08 15:33:48 +0200

    New updates

---
+++ Chapter2_Page25_assingment.py
@@ -0,0 +1,9 @@
+def message_1():
+    """ Harjoitus 2-1"""
+    message = "Hello Python World"
+
+    return message
+print(message_1())
+
+
+     
--- hello_world.py
+++ hello_world.py
@@ -2,4 +2,14 @@ message = "Hello Python World !"
 print(message)
 
 Hello = "Python Crash Course world!"
-print(mesage)
+print(Hello)
+
+
+
+def message_1():
+    """ Markus v3.0"""
+    message = "Hello Python World"
+
+    return message
+print(message_1())
+
