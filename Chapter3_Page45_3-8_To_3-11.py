0d553aea1775a34906b8ffd9becf253517393204
Markus Kaprio <markus.kaprio@edunord.se>
2025-12-09 14:18:37 +0200

    Added Chapter 3 own example

---
+++ Chapter3_Page45_3-8_To_3-11.py
@@ -0,0 +1,13 @@
+# Here you can find the assigments from Exercises 3-8 through 3-11.
+
+#Example code to just try out 
+
+bicycles = ['car', 'building', 'boat', 'this stuff is just too easy...']
+print(bicycles)
+
+
+
+
+# 3-8: number_analysis.py
+
+ 
