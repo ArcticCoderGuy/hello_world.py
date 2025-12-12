bf6f156304373f0fe89272e4a167afc64eaf293f
Markus Kaprio <markus.kaprio@edunord.se>
2025-12-12 13:29:18 +0200

    Made and checked the assignment

--- Chapter4_exercises.py
+++ Chapter4_exercises.py
@@ -13,6 +13,16 @@ for magician in magicians:
 for magician in magicians:
     print(f"{magician.title()}, that was an awesome trick you did !")
     print(f"I can't wait to see your next trick, {magician.title()}.\n")    
-print("Thank you, everyone. That was a great magic show!")
+    
+    
+print("Thank you, everyone. That was such a great magic show!") # And I did indet this line to be outside the for-loop, as instructuion says.
 
-21
+# Page 56 - 4-1: Pizzas
+
+pizzas = ['Margherita', 'Pepperoni', 'Hawaiian']        
+for pizza in pizzas:
+    
+    print(f"I like {pizza} pizza.")
+    
+ 
+print("\nI really love all of these pizzas!")  # Indented outside the for-loop.
