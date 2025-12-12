54060633d6ea35e5f0a79dce1de4a32b239300e0
Markus Kaprio <markus.kaprio@edunord.se>
2025-12-12 13:12:38 +0200

    extended For-loopu and de-intendentatiomn of the Thank you-note

--- Chapter4_exercises.py
+++ Chapter4_exercises.py
@@ -8,4 +8,10 @@ for magician in magicians:
     print(f"{magician.title()}, that was an awesome trick you did !")
     
     
-    
+    #Page 52
+    magicians = ["Peter", "Steve", "Colin"]
+for magician in magicians:
+    print(f"{magician.title()}, that was an awesome trick you did !")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")    
+print("Thank you, everyone. That was a great magic show!")
+
