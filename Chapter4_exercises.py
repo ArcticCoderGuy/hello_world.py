42b73cd16650dd34e6af09d18e45b7dc46da768e
Markus Kaprio <markus.kaprio@edunord.se>
2025-12-12 11:45:08 +0200

    Chapter 4 started and doing the exercises

---
+++ Chapter4_exercises.py
@@ -0,0 +1,10 @@
+
+# Here you can find all the exercisese in Chapter 4
+
+# The Magician.py-task on page 50
+
+magicians + ["Peter", "Steve", "Colin"]
+for magician in magicians:
+    print(f"{magician], that was an awesome trick you did !")}")
+    
+    
