13e5c3e38878c1db095a72c0786488177c91ece9
Markus Kaprio <markus.kaprio@edunord.se>
2025-12-13 19:31:53 +0200

    Task 5-9 No users

--- Chapter5_exercises.py
+++ Chapter5_exercises.py
@@ -219,3 +219,20 @@ for username in usernames:
         print("Hello admin, would you like to see a status report?")
     else:
         print(f"Hello {username.title()}, thank you for logging in again.")
+
+
+# Task 5-9: No Users
+
+usernames = ['admin', 'markus', 'maria', 'ville', 'guest']
+
+# Tyhjennetään lista tarkoituksella:
+usernames.clear()
+
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {username.title()}, thank you for logging in again.")
+else:
+    print("We need to find some users!")
