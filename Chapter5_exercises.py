4446ebc9a58d8449f5d28c8dcde82dbf5970194e
Markus Kaprio <markus.kaprio@edunord.se>
2025-12-13 17:04:17 +0200

    The inequality operator task

--- Chapter5_exercises.py
+++ Chapter5_exercises.py
@@ -6,10 +6,25 @@
 cars = ["F1 Mclaren", "Ferrari", "Red Bull", "Mercedes", "Alpine"]
 
 for variable in cars:    
-      if variable == "bmw":
+      if variable == "Alpine":
           print(variable.upper())
       else:
           print(variable.title())
           
 # Example with if-else 
+
+
+#Applied page 74 example toppings.py and the != ineqaulity operator 
+
+
+we_want = 'Linux'
+
+we_do_not_want = 'Windows Vista'
+    
+if we_want != we_do_not_want:
+    print(f"hold the {we_do_not_want.upper()} !! ")
+    
+        
+
+
 
