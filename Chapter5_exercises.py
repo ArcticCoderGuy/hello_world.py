cf7d1fc961a362cd78752474b2151d103fd51f63
Markus Kaprio <markus.kaprio@edunord.se>
2025-12-13 11:33:11 +0200

    Started with a simple example of if-else structure

--- Chapter5_exercises.py
+++ Chapter5_exercises.py
@@ -1,4 +1,15 @@
 
 # Here are the relevant list method examples from Chapter 5 exercises 
 
+# My introduction to if-statements:
+
+cars = ["F1 Mclaren", "Ferrari", "Red Bull", "Mercedes", "Alpine"]
+
+for variable in cars:    
+      if variable == "bmw":
+          print(variable.upper())
+      else:
+          print(variable.title())
+          
+# Example with if-else 
 
