7fd48433b59e00023c001fef84fa7913be6f0525
NorthFoxAI <markus.kaprio@edunord.se>
2025-12-15 10:28:33 +0100

    Made alien.py and a small script

--- Chapter6_exercises.py
+++ Chapter6_exercises.py
@@ -1,2 +1,15 @@
 # Here are all the Chapter6 tasks and things
 
+
+
+# alien.py task 1
+
+alien_0 = {'color' : 'green' , 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+              
+              
+              
+               
+
