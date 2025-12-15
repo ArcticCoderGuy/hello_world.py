4bdd81b9d93ffc09ba8ee74ca8ad0adac3ecf16e
NorthFoxAI <markus.kaprio@edunord.se>
2025-12-15 14:01:11 +0100

    Made an empty dictionary

--- Chapter6_exercises.py
+++ Chapter6_exercises.py
@@ -10,12 +10,27 @@ print(alien_0['color'])
 print(alien_0['points'])
 
 
-#My own example
+#My own example on key-values in multiple things
 
-cabin_in_yllas = {'made out of': 'wood', 
-                  'Log-type': 'Spruce' }
+cabin_in_yllas = {'made out of': 'wood', 'Log-type': 'Spruce', 'Location': '342.344.321.112', 'Is in AirBbN': 'Yes' }
 print(cabin_in_yllas['made out of'])
 print(cabin_in_yllas['Log-type'])
+print(cabin_in_yllas['Location'])
+print(cabin_in_yllas['Is in AirBbN'])
+
+# The book showed an example where it had just an empty dictionary
+
+cabin_in_yllas = {}
+
+cabin_in_yllas['Is in the parrish of Kittilä'] = 'We are in Kittilä parrish' 
+cabin_in_yllas['Has how many kilometers of ski-tracks'] = '432km'
+
+print(cabin_in_yllas)
+
+
+
+
+
 
 
 
